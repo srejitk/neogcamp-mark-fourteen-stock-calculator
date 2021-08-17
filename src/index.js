a80abefb635404bba9a58eb79fdcc86d977d1706
Sreejith K <kumar.edwardcullen.sreejith80@gmail.com>
2021-08-18 00:03:05 +0530

    feat : Added emojis to results

--- src/index.js
+++ src/index.js
@@ -5,11 +5,11 @@ var buy_Qty = document.querySelector("#buy_qty");
 var sell_Price = document.querySelector("#sell_price");
 var chkBtn = document.querySelector("#chk_btn");
 var output = document.querySelector("#output-id");
-var bg = document.querySelector("#app");
+
 var gif_sad = document.querySelector("#gif-sad");
 var gif_happy = document.querySelector("#gif-happy");
 var gif_meh = document.querySelector("#gif-meh");
-var gif_m = document.querySelector("#gif-meh");
+var gif_sigh = document.querySelector("#gif-sigh");
 
 chkBtn.addEventListener("click", clickHandler);
 
@@ -21,7 +21,10 @@ function clickHandler() {
     calculateReturns(ip, qty, sp);
   } else {
     output.innerText = `Risk hai toh Ishq hai.\n Enter valid values first Mr.Buffet.`;
-    gif_meh.style.display = "block;";
+    gif_sigh.style.display = "block";
+    gif_sad.style.display = "none";
+    gif_happy.style.display = "none";
+    gif_meh.style.display = "none";
   }
 }
 
@@ -34,17 +37,26 @@ function calculateReturns(initial, qty, current) {
     var loss = (buy - sell) * share_qty;
     var loss_perc = ((loss * 100) / buy).toFixed(2);
     output.innerText = `Ouch! You made a loss of ${loss}, that's ${loss_perc}%.`;
-    gif_sad.style.display = "block;";
+    gif_sad.style.display = "block";
+    gif_happy.style.display = "none";
+    gif_meh.style.display = "none";
+    gif_sigh.style.display = "none";
+
     if (loss_perc > 50) {
-      bg.style.background = "red";
     }
   } else if (sell > buy) {
     var profit = (sell - buy) * share_qty;
     var profit_perc = ((profit * 100) / buy).toFixed(2);
-    output.innerText = `Woah! You made a profit of ${profit}, that's ${profit_perc}%.`;
-    gif_happy.style.display = "block;";
-    bg.style.background = "green";
+    output.innerText = `Ouch! You made a profit of ${profit}, that's ${profit_perc}%.`;
+    gif_sad.style.display = "none";
+    gif_happy.style.display = "block";
+    gif_meh.style.display = "none";
+    gif_sigh.style.display = "none";
   } else {
     output.innerText = `Okay, Who asked you to invest in nothingness?`;
+    gif_sad.style.display = "none";
+    gif_happy.style.display = "none";
+    gif_meh.style.display = "none";
+    gif_sigh.style.display = "block";
   }
 }
